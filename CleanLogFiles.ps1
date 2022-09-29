cc45c9f96c0c7373c6ab215a6bf75331a3ed6f9d
simileexpert <simileoluwa.babalola@outlook.com>
2022-09-29 15:37:37 +0100

    basic script

--- CleanLogFiles.ps1
+++ CleanLogFiles.ps1
@@ -1,19 +1,12 @@
-# Filename : CleanLogs.ps1
-# Purpose : Deletes all log files older than 30 days
-
-
 # Days to keep logs files.
 $LogsRetention = 30
 # Location of files to be deleted
-$LogPath = "C:\Users"
-
+$LogPath = ""
 # Make the retention times negative.
 $LogsRetention = $LogsRetention * -1
-
 # Delete the old Logs files.
 $Logs = (Get-ChildItem $LogPath | `
     Where-Object{$_.LastWriteTime -lt (Get-Date).AddDays($LogsRetention)})
 $Logs | Remove-Item
-# Write Message to Console
- Write-Host "Files Deleted"
+
 
